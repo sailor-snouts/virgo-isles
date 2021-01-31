52d0588489c6a87d83de353753ea9d46ac09f26d
Robert Parker <diaboloshogunate@gmail.com>
2021-01-30 21:46:48 -0800

    model updates and playericon rotation

---
+++ Assets/Scripts/HUDMenu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDMenu : MonoBehaviour
+{
+    [SerializeField] private Image minimap = null;
+    [SerializeField] private Image playerIcon = null;
+    [SerializeField] private Transform player = null;
+
+    private void Update()
+    {
+        Vector3 rotation = player.eulerAngles;
+        rotation.x = 0;
+        rotation.z = 360 - rotation.y;
+        rotation.y = 0;
+        this.playerIcon.transform.rotation = Quaternion.Euler(rotation);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Random = UnityEngine.Random;
 
 public class PlayerController : MonoBehaviour
 {
@@ -14,6 +13,17 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private GameObject flag = null;
     [SerializeField] private float boatRotationSpeed = 10f;
     [SerializeField] private float sailsRotationSpeed = 10f;
+    [SerializeField] private float maxVelocity = 10f;
+    private float maxVelocitySquared = 100f;
+    private Vector3 wind = Vector3.zero;
+    private float timeToWindChange = 0;
+    private Rigidbody rb = null;
+
+    private void Awake()
+    {
+        this.rb = this.GetComponent<Rigidbody>();
+        this.maxVelocitySquared = this.maxVelocity * this.maxVelocity;
+    }
 
     public void RotateSailsClockwise(InputAction.CallbackContext context)
     {
@@ -37,6 +47,32 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        this.ChangeWind();
+        this.RotateSails();
+        this.RotateFlag();
+    }
+
+    private void FixedUpdate()
+    {
+        float speed = Vector3.Dot(this.sails.transform.forward.normalized, this.wind.normalized) * this.maxVelocity;// 0 if perpendicular, 1 if same, -1 if opposite
+        if (speed <= 0) speed *= -0.25f;// only 25% force if sail is facing wrong direction
+        this.rb.velocity = this.sails.transform.forward * speed;
+    }
+
+    private void ChangeWind()
+    {
+        this.timeToWindChange -= Time.deltaTime;
+        if (this.timeToWindChange > 0) return;
+        float velocity = Random.Range(0f, 10f);
+        float angle = Random.Range(0f, 2 * Mathf.PI);
+        Vector2 windDirection = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+        windDirection.Normalize();
+        this.wind = new Vector3(windDirection.x, 0, windDirection.y) * velocity;
+        this.timeToWindChange = Random.Range(300f, 3600f);
+    }
+    
+    private void RotateSails()
+    {
         if (this.sailsDirection != Direction.NONE)
         {
             Vector3 sailsRotation = this.sailsDirection == Direction.CLOCKWISE ? Vector3.up : Vector3.down;
@@ -45,7 +81,12 @@ public class PlayerController : MonoBehaviour
         if (this.boatDirection != Direction.NONE)
         {
             Vector3 boatRotation = this.boatDirection == Direction.CLOCKWISE ? Vector3.up : Vector3.down;
-            this.gameObject.transform.Rotate(boatRotation * (Time.deltaTime * this.sailsRotationSpeed));
+            this.gameObject.transform.Rotate(boatRotation * (Time.deltaTime * this.boatRotationSpeed));
         }
+    }
+
+    private void RotateFlag()
+    {
+        this.flag.transform.LookAt(this.flag.transform.position - this.wind);
     }
 }
